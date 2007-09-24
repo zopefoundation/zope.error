833f6790c7674351b6ceddeeee447d619980db57
Roger Ineichen <roger@projekt01.ch>
2007-09-24 20:42:39 +0000

    Prepare zope.error

---
+++ README.txt
@@ -0,0 +1,1 @@
+This package provides an error reporting utility which is able to store errors.
---
+++ setup.py
@@ -0,0 +1,63 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.error package
+
+$Id: setup.py 79889 2007-09-24 16:35:25Z rogerineichen $
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.error',
+    version = '3.5.0',
+    author='Zope Corporation and Contributors',
+    author_email='zope3-dev@zope.org',
+    description = "An error reporting utility for Zope3",
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license='ZPL 2.1',
+    keywords = "zope3 error",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='http://cheeseshop.python.org/pypi/zope.error',
+	packages=find_packages('src'),
+	package_dir = {'': 'src'},
+    extras_require=dict(
+        test=['zope.app.testing']),
+    namespace_packages=['zope',],
+    install_requires=['setuptools',
+                      'zope.exceptions',
+                      'zope.app.appsetup',
+                      'zope.publisher',
+                      'zope.app.container',
+                      ],
+    include_package_data = True,
+
+    zip_safe = False,
+    )
