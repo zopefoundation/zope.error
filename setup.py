c5380784c43225d173db94dad249a26686f1278b
Thomas Lotze <tl@gocept.com>
2009-09-29 05:32:46 +0000

    Preparing release 3.7.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.error',
-    version = '3.6.1dev',
+    version = '3.7.0',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description = "An error reporting utility for Zope3",
