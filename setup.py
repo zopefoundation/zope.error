7ba2883e0e89bf024c7cd203bda18c77220f4ee8
Michael Howitz <mh@gocept.com>
2010-09-25 12:42:33 +0000

    Back to development: 3.7.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.error',
-    version='3.7.1',
+    version='3.7.2dev',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description = "An error reporting utility for Zope3",
