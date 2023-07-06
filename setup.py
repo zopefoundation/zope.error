b390f39d07aff3e29be8858456a4c5d20228b7a7
Michael Howitz <icemac@gmx.net>
2023-07-06 08:13:11 +0200

    Back to development: 5.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def read(*rnames):
 
 
 setup(name='zope.error',
-      version='5.0',
+      version='5.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description="An error reporting utility for Zope3",
